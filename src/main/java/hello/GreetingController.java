c4fcdc2eeb2a40c1cb56d4e0672efee79d352997
Clement <clemetsoullard@yahoo.fr>
2016-06-16 19:15:22 +0000

    CouOUC

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,7 +4,8 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.io.File;
+import java.io.FileOutputStream;
 @RestController
 public class GreetingController {
 
@@ -12,8 +13,11 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    public Greeting greeting(@RequestParam(value="value", defaultValue="90") 	Integer value) {
+		File file=new File("/tmp/scheduler/CD");
+        FileOutputStream fos=new FileOutputStream(file);
+		fos.print(value);
+		return new Greeting(counter.incrementAndGet(),
+                           "Hehe");
     }
 }
