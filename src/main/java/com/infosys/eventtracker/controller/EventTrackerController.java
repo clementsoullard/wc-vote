20b10e4da467d0ba17d867a0ddb9c6d03b25c650
Clement Soullard <clementsoullard@yahoo.fr>
2016-08-23 18:15:45 +0200

    Ajout de la securité

--- src/main/java/com/infosys/eventtracker/controller/EventTrackerController.java
+++ src/main/java/com/infosys/eventtracker/controller/EventTrackerController.java
@@ -1,27 +1,14 @@
 package com.infosys.eventtracker.controller;
 
-import java.util.Date;
-
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.infosys.eventtracker.service.ParticipationRepository;
-import com.infosys.eventtracker.FileService;
-import com.infosys.eventtracker.TvCheckScheduler;
-import com.infosys.eventtracker.dto.CreditResult;
-import com.infosys.eventtracker.dto.Participation;
-import com.infosys.eventtracker.dto.PunitionResult;
 import com.infosys.eventtracker.dto.graph.Wrapper;
-import com.infosys.eventtracker.object.BonPoint;
-import com.infosys.eventtracker.object.LogEntry;
-import com.infosys.eventtracker.object.TVStatus;
-import com.infosys.eventtracker.service.LogRepository;
 import com.infosys.eventtracker.service.LogRepositoryImpl;
+import com.infosys.eventtracker.service.ParticipationRepository;
 
 @RestController
 public class EventTrackerController {
---
+++ src/main/java/com/infosys/eventtracker/security/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.infosys.eventtracker.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/manager").setViewName("manager");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
