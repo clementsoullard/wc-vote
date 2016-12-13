983edbdcb2756299a5d8049e8029f3e7aa1944d9
Clement_Soullard <Clement_Soullard@ENGLON127758L.ad.infosys.com>
2016-12-13 22:40:17 +0100

    Commit intermediaire

---
+++ src/main/webapp/caisse/caisse.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+
+angular.module('managerApp.caisse', ['ngRoute','angularFileUpload'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/caisse', {
+    templateUrl: 'caisse/caisse.html',
+    controller: 'caisseMgrCtrl'
+  })
+   ;
+}])
+
+.controller('caisseMgrCtrl',  ['$scope','$http', function($scope,$http) {
+
+	/**
+	 * Insert a new entry function
+	 */
+			
+	 $scope.insert = function (line) {
+
+		 $http.post('ws/line-caisse/',line).
+	        	success(function(data) {
+	     	  	$scope.message='Thanks for registering.';
+	     	  	$scope.line={};
+	         	$scope.error=false;
+	            list();
+	        }).
+			error(function(data) {
+	     	  	$scope.message='An issue occured.';
+	     	  	$scope.participant={};
+	     	  	$scope.error=true;
+			})
+		};
+		/**
+		 * List the entries
+		 */		
+		function list(){
+				 $http.get('ws/line-caisse/').
+			      success(function(data) {
+			            $scope.linesCaisse = data.line;
+			      });				 
+			}
+
+
+}])
